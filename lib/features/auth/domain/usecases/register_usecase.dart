d913b48737c87c1f5403c01a86712afe88e92bc0
SushantshtL0L <shtsushant2027@gmail.com>
2026-01-17 16:25:50 +0545

    feat(auth): add name and confirmPassword to register usecase

--- lib/features/auth/domain/usecases/register_usecase.dart
+++ lib/features/auth/domain/usecases/register_usecase.dart
@@ -3,28 +3,33 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sneak_fit/core/error/failure.dart';
 import 'package:sneak_fit/core/usecase/app_usecase.dart';
-import 'package:sneak_fit/features/auth/data/repositories/auth_repository.dart';
+import 'package:sneak_fit/features/auth/data/repositories/auth_repository_impl.dart';
 import 'package:sneak_fit/features/auth/domain/entities/auth_entity.dart';
 import 'package:sneak_fit/features/auth/domain/repositories/auth_repository.dart';
 
 /// Params for Register Usecase
 class RegisterUsecaseParams extends Equatable {
+  final String name;
   final String userName;
   final String email;
   final String password;
-  final String phoneNumber; // added phoneNumber
+  final String confirmPassword;
+  final String phoneNumber;
   final String? profileImage;
 
   RegisterUsecaseParams({
+    required this.name,
     required this.userName,
     required this.email,
     required this.password,
+    required this.confirmPassword,
     required this.phoneNumber,
     this.profileImage,
   });
 
   @override
-  List<Object?> get props => [userName, email, password, phoneNumber, profileImage];
+  List<Object?> get props =>
+      [name, userName, email, password, confirmPassword, phoneNumber, profileImage];
 }
 
 /// Riverpod provider for RegisterUsecase
@@ -44,6 +49,7 @@ class RegisterUsecase implements UsecaseWithParams<bool, RegisterUsecaseParams> 
   Future<Either<Failure, bool>> call(RegisterUsecaseParams params) {
     // Map params to AuthEntity
     final authEntity = AuthEntity(
+      name: params.name,
       userName: params.userName,
       email: params.email,
       password: params.password,
