7ba1d938eb31aa9baf71f916e180029b552af49c
SushantshtL0L <shtsushant2027@gmail.com>
2026-02-25 00:34:47 +0545

    update android config

--- android/app/src/main/kotlin/com/sushantshrestha/sneak_fit/MainActivity.kt
+++ android/app/src/main/kotlin/com/sushantshrestha/sneak_fit/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.sushantshrestha.sneak_fit
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
