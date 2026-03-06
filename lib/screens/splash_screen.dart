0221e12f30af3dda51cc98e8d666b4c816b8e359
SushantshtL0L <shtsushant2027@gmail.com>
2026-03-07 02:12:47 +0545

    add new splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,15 +1,19 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sneak_fit/core/storage/user_session_service.dart';
 import 'package:sneak_fit/screens/onboarding/onboarding_main.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class SplashScreen extends StatefulWidget {
+
+class SplashScreen extends ConsumerStatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  ConsumerState<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends ConsumerState<SplashScreen> {
   Timer? _timer;
 
   @override
@@ -20,12 +24,18 @@ class _SplashScreenState extends State<SplashScreen> {
       _timer = Timer(const Duration(seconds: 2), () {
         if (!mounted) return;
 
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (context) => const OnboardingMain(),
-          ),
-        );
+        final sessionService = ref.read(userSessionServiceProvider);
+        
+        if (sessionService.isLoggedIn()) {
+          Navigator.pushReplacementNamed(context, '/dashboard');
+        } else {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const OnboardingMain(),
+            ),
+          );
+        }
       });
     });
   }
@@ -39,12 +49,17 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox.expand(
-        child: Image.asset(
-          "assets/images/splash.png",
-          fit: BoxFit.cover,
+      backgroundColor: Colors.black, // Matching the background of your splash2 image
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(40.r), // Responsive padding
+          child: Image.asset(
+            "assets/images/splash2.png",
+            fit: BoxFit.contain, // Ensures logo is never cropped on any device
+          ),
         ),
       ),
     );
+
   }
 }
